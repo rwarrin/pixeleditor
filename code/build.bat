76d3b3a9e5e3b5809c47efd02d315e792128f0c3
Rick Warrington <phial1020@gmail.com>
2018-02-19 22:19:05 -0700

    Update compiler flags and add release settings

--- code/build.bat
+++ code/build.bat
@@ -1,7 +1,10 @@
 @echo off
 
-SET CompilerFlags=/nologo /Z7
+SET CompilerFlags=/nologo /Zi /fp:fast /Wall /W1
 SET LinkerFlags=/incremental:no user32.lib gdi32.lib Comdlg32.lib
+
+REM Release Settings
+REM SET CompilerFlags=/nologo /Oi /Ox /fp:fast /favor:INTEL64 /w /MT
 
 if not exist ..\build mkdir ..\build
 pushd ..\build
